341e40ce935eedd88d5638b5c56fdf57cd98b65e
brokenRoX <github@lucas.hruschka.org>
2022-06-20 00:29:05 +0200

    add eventlistener to all fields

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 const game = {
     "boardsize": [10, 10],
+    "boardIds": [],
     "drawboard": function(boardDivId) {
         const board = document.getElementById(boardDivId)
         for (let y = 0; y < this.boardsize[1]; y++) {
@@ -7,14 +8,25 @@ const game = {
                 const field = document.createElement("div")
                 field.classList.add("field")
                 field.id = boardDivId + "_" + x + "_" + y
+
+                field.addEventListener("click", function() { 
+                    game.onClick(boardDivId, x, y)
+                })
                 board.appendChild(field)
             }
         }
+    },
+    "onClick": function(boardId, x, y) {
+        const isOurs = boardId === this.boardIds[0]
+        if (isOurs) return
+        
+        console.log("clicked " + (isOurs) + x + y)
     }
-
 }
 
 function initialize(myShipsDivId, otherShipsDivId) {
+    game.boardIds.push(myShipsDivId)
+    game.boardIds.push(otherShipsDivId)
     game.drawboard(myShipsDivId)
     game.drawboard(otherShipsDivId)
 }
