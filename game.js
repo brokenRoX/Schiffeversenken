031a4086793a73c6ae3f5f2e12e76b95e9dda26a
Lucas <github@lucas.hruschka.org>
2022-06-22 22:16:50 +0200

    update the UI with states as classes

--- game.js
+++ game.js
@@ -1,7 +1,17 @@
+const state = {
+    water: "water",
+    ship: "ship",
+    hit: "hit",
+    miss: "miss",
+    unknown: "unknown"
+}
+
 const game = {
-    "boardsize": [10, 10],
-    "boardIds": [],
-    "drawboard": function(boardDivId) {
+    boardsize: [10, 10],
+    boardIds: [],
+    ourState: [],
+    otherState: [],
+    drawboard: function(boardDivId) {
         const board = document.getElementById(boardDivId)
         for (let y = 0; y < this.boardsize[1]; y++) {
             for (let x = 0; x < this.boardsize[0]; x++) {
@@ -16,11 +26,25 @@ const game = {
             }
         }
     },
-    "onClick": function(boardId, x, y) {
+    onClick: function(boardId, x, y) {
         const isOurs = boardId === this.boardIds[0]
         if (isOurs) return
         
         console.log("clicked " + (isOurs) + x + y)
+    },
+    updateUiBoard: function(boardId, boardState) {
+        for (let y = 0; y < this.boardsize[1]; y++) {
+            for (let x = 0; x < this.boardsize[0]; x++) {
+                const fieldId = boardId + "_" + x + "_" + y
+                const currentField = document.getElementById(fieldId)
+                currentField.classList.remove(state.water, state.ship, state.hit, state.miss)
+                currentField.classList.add(boardState[y][x])
+            }
+        }
+    },
+    updateUi: function() {
+        this.updateUiBoard(this.boardIds[0], this.ourState)
+        this.updateUiBoard(this.boardIds[1], this.otherState)
     }
 }
 
@@ -29,5 +53,17 @@ function initialize(myShipsDivId, otherShipsDivId) {
     game.boardIds.push(otherShipsDivId)
     game.drawboard(myShipsDivId)
     game.drawboard(otherShipsDivId)
+
+    for (let y = 0; y < game.boardsize[1]; y++) {
+        let ourRow = []
+        let otherRow = []
+        for (let x = 0; x < game.boardsize[0]; x++) {
+            ourRow.push(state.water)
+            otherRow.push(state.unknown)
+        }
+        game.ourState.push(ourRow)
+        game.otherState.push(otherRow)
+    }
+    game.updateUi()
 }
 
