a1a0b4d18a37fa4656f1f662ac54899790170d26
laurenz2303 <101352336+laurenz2303@users.noreply.github.com>
2022-07-30 12:23:53 +0200

    changed layout in game.html styled "start button" changed music and added music controls

--- client/style.js
+++ client/style.js
@@ -5,3 +5,14 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
     loader.style.display = "none";
 })
+
+
+//toggles music and sound controls
+//Needs to be checked
+function changeIcon(anchor) {
+    var icon = anchor.querySelector("i");
+    icon.classList.toggle('fa-plus');
+    icon.classList.toggle('fa-minus');
+  
+     anchor.querySelector("span").textContent = icon.classList.contains('fa-plus') ? "Read more" : "Read less";
+  }
