43bc43711b2638a2be995d48d118e521bd5a63ce
laurenz2303 <101352336+laurenz2303@users.noreply.github.com>
2022-07-30 21:33:58 +0200

    changed board background

--- client/index.js
+++ client/index.js
@@ -1,8 +1,15 @@
+//Loader
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+})
+
 //Styling
 let button_a = document.querySelector('#play');
 let background_picture = document.querySelector('#BG');
 
-function zoom(){
+function zoom() {
   background_picture.classList.add('background_size');
   button_a.classList.add('smooth_button');
-  };
+};
